7a0748b0dffd3d39af1001f02deca5ea56b45299
Matthew Higley <mhigley@MattyBook-Pro.local>
2015-04-19 15:45:22 -0700

    added watch to default task

--- gulpfile.js
+++ gulpfile.js
@@ -49,7 +49,7 @@ gulp.task('watch', function(){
 	gulp.watch('components/sass/*.scss', ['compass']);	
 });
 
-gulp.task('default', ['coffee', 'js', 'compass']);
+gulp.task('default', ['coffee', 'js', 'compass', 'watch']);
 
 gulp.task('sass', function(){
 	gulp.src(sassSources)
